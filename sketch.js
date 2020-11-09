aff79d763ebd7e70a02eb21712546721e1f88c87
deepa0607 <deepayasoda@gmail.com>
2020-11-09 12:15:09 +0530

    added platform

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg;
+var pf;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -34,7 +35,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+   pf= new Ground(150,300,300,170);
 }
 
 function draw(){
@@ -57,6 +58,7 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    pf.display();
 
     bird.display();
 }
